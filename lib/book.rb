be3637fdeeba5f00c5ef420144742651bfb7eb96
AlecGrey <alecgrey.coding@gmail.com>
2020-10-08 12:28:41 -0700

    completed lib/book.rb

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
 
+class Book
 
+    attr_accessor :author, :page_count, :genre, :page_number
+    attr_reader :title
+
+    def initialize(title)
+        @title = title
+        # @page_count = page_count
+        @page_number = 0
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
